9de0fd08967a4663e8525a641242b945678fb270
Marc-Andre Roy <marc596@gmail.com>
2022-02-10 15:06:20 -0500

    Add Hangfire in service3, fix a few more things

---
+++ WebAppApi3/Authorizations/HangfireDashboardAuthorizationFilter.cs
@@ -0,0 +1,12 @@
+﻿using Hangfire.Dashboard;
+
+namespace WebAppApi3.Authorizations
+{
+    public class HangfireDashboardAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext dashboardContext)
+        {
+            return true;
+        }
+    }
+}
--- WebAppApi3/Program.cs
+++ WebAppApi3/Program.cs
@@ -1,3 +1,6 @@
+using Hangfire;
+using WebAppApi3.Authorizations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Hangfire
+builder.Services.AddHangfire(x => x.UseSqlServerStorage("Server=mssql-dev;Database=HangFire;User Id=sa;Password=SomeHardPasswordNotGonnaLie2022!;"));
+builder.Services.AddHangfireServer();
 
 var app = builder.Build();
 
@@ -19,6 +26,20 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+// Middleware to automatically handle virtual paths. Reference: https://github.com/HangfireIO/Hangfire/issues/1368#issuecomment-472785155
+app.Use((context, next) =>
+{
+    var pathBase = new PathString(context.Request.Headers["X-Forwarded-Prefix"]);
+    if (pathBase != null)
+        context.Request.PathBase = new PathString(pathBase.Value);
+    return next();
+});
+
+app.UseHangfireDashboard("/hangfire", new DashboardOptions
+{
+    Authorization = new[] { new HangfireDashboardAuthorizationFilter() }
+});
 
 app.MapControllers();
 
